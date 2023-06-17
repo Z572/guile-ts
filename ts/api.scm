8296330c2996dd19567ec93228c1809cbc2549c3
Z572 <zhengjunjie@iscas.ac.cn>
2023-06-17 20:44:22 +0800

    ts/api.scm: sort module and export.

--- ts/api.scm
+++ ts/api.scm
@@ -1,56 +1,56 @@
 (define-module (ts api)
+  #:use-module (oop goops)
   #:use-module (srfi srfi-26)
   #:use-module (system foreign)
-  #:use-module (system foreign-object)
   #:use-module (system foreign-library)
-  #:use-module (oop goops)
+  #:use-module (system foreign-object)
   #:export (<ts-parser>
             <ts-range>
-            ts-tree?
-            ts-node?
-            ts-parser-new
-            ts-parser-reset!
-            ts-parser-language
-            ts-parser-included-ranges
-            ts-parser-timeout
             get-ts-language-from-file
-            ts-parser-parse-string
-            ts-tree-root-node
-            ts-tree-language
-            ts-tree-delete
-            ts-tree-copy
-            ts-node-field-name-for-child
-            ts-node-sexp
-            ts-node-start-byte
-            ts-node-start-point
-            ts-node-end-byte
-            ts-node-end-point
-            ts-node-null?
-            ts-node-named?
-            ts-node-missing?
-            ts-node-extra?
-            ts-node-has-changes?
-            ts-node-has-error?
-            ts-node-parent
-            ts-node-child
-            ts-node-child-count
-            ts-node-child-by-field-name
-            ts-node-named-child-count
-            ts-node-named-child
-            ts-node-type
-            ts-node-first-child-for-byte
-            ts-node-first-named-child-for-byte
             ts-language-field-count
             ts-language-field-name-for-id
             ts-language-version
+            ts-node-child
+            ts-node-child-by-field-name
+            ts-node-child-count
             ts-node-childs
+            ts-node-end-byte
+            ts-node-end-point
+            ts-node-extra?
+            ts-node-field-name-for-child
+            ts-node-first-child-for-byte
+            ts-node-first-named-child-for-byte
+            ts-node-has-changes?
+            ts-node-has-error?
+            ts-node-missing?
+            ts-node-named-child
+            ts-node-named-child-count
             ts-node-named-childs
-            ts-tree-cursor-new
-            ts-tree-cursor-delete
-            ts-tree-cursor-reset!
-            ts-tree-cursor-current-node
+            ts-node-named?
+            ts-node-null?
+            ts-node-parent
+            ts-node-sexp
+            ts-node-start-byte
+            ts-node-start-point
+            ts-node-type
+            ts-node?
+            ts-parser-included-ranges
+            ts-parser-language
+            ts-parser-new
+            ts-parser-parse-string
+            ts-parser-reset!
+            ts-parser-timeout
+            ts-tree-copy
+            ts-tree-cursor-copy
             ts-tree-cursor-current-field-name
-            ts-tree-cursor-copy))
+            ts-tree-cursor-current-node
+            ts-tree-cursor-delete
+            ts-tree-cursor-new
+            ts-tree-cursor-reset!
+            ts-tree-delete
+            ts-tree-language
+            ts-tree-root-node
+            ts-tree?))
 
 (eval-when (expand load eval)
   (load-extension "libguile_ts" "init_ts")
